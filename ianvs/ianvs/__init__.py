2e3c8c3378f7092d6563a7d9f19b5520c72c3e53
Nathan Hughes <nathan.h.hughes@gmail.com>
2026-08-23 17:17:06 +0000

    add initial cut at python bag read interface

--- ianvs/ianvs/__init__.py
+++ ianvs/ianvs/__init__.py
@@ -3,6 +3,8 @@ import sensor_msgs.msg
 from typing import Optional
 import numpy as np
 
+import imageio.v3
+
 
 def get_image(msg: sensor_msgs.msg.Image) -> Optional[np.ndarray]:
     """Return appropriate image from message if possible."""
@@ -12,3 +14,13 @@ def get_image(msg: sensor_msgs.msg.Image) -> Optional[np.ndarray]:
 
     shape = (msg.height, msg.width, info.channels)
     return np.squeeze(np.frombuffer(msg.data, dtype=info.dtype_str).reshape(shape))
+
+
+def _parse_image(msg):
+    if msg is None:
+        return None
+
+    if "CompressedImage" in type(msg).__name__:
+        return imageio.v3.imread(msg.data.tobytes())
+
+    return get_image(msg)
